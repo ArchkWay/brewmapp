8925bd2d68754113e4ecdb70adcbe2904ded300a
Kras <xpusher@yandex.ru>
2017-11-24 07:52:40 +0400

    dev

--- app/src/main/java/com/brewmapp/presentation/view/impl/activity/PostDetailsActivity.java
+++ app/src/main/java/com/brewmapp/presentation/view/impl/activity/PostDetailsActivity.java
@@ -1,9 +1,16 @@
 package com.brewmapp.presentation.view.impl.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.CharacterStyle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -17,6 +24,8 @@ import com.brewmapp.presentation.presenter.contract.PostDetailsPresenter;
 import com.brewmapp.presentation.view.contract.PostDetailsView;
 import com.brewmapp.presentation.view.impl.widget.ShareLikeView;
 import com.squareup.picasso.Picasso;
+
+import org.xml.sax.XMLReader;
 
 import java.util.List;
 
@@ -46,6 +55,7 @@ public class PostDetailsActivity extends BaseActivity implements PostDetailsView
     @Inject    PostDetailsPresenter presenter;
 
     private Post post;
+    private Html.TagHandler htmlTagHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +66,69 @@ public class PostDetailsActivity extends BaseActivity implements PostDetailsView
     @Override
     protected void initView() {
         enableBackButton();
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+        text.setLinksClickable(true);
+        htmlTagHandler= new Html.TagHandler() {
+            int cntOpen=0;
+            int cntClose=0;
+            @Override
+            public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+                if(tag.startsWith("mybr")){
+                    if(opening) {
+                        if (cntOpen == 0)
+                            processSpan(opening, output, new AppearanceSpan(cntOpen++));
+                    }else{
+                        if (cntClose == 0)
+                            processSpan(opening, output, new AppearanceSpan(cntClose++));
+
+                    }
+                }
+            }
+        };
+
+    }
+    void processSpan(boolean opening, Editable output, Object span) {
+        int len = output.length();
+        if (opening) {
+            output.setSpan(span, len, len, Spannable.SPAN_MARK_MARK);
+        } else {
+            Object[] objs = output.getSpans(0, len, span.getClass());
+            int where = len;
+            if (objs.length > 0) {
+                for(int i = objs.length - 1; i >= 0; --i) {
+                    if (output.getSpanFlags(objs[i]) == Spannable.SPAN_MARK_MARK) {
+                        where = output.getSpanStart(objs[i]);
+                        output.removeSpan(objs[i]);
+                        break;
+                    }
+                }
+            }
+
+            if (where != len) {
+                output.setSpan(span, where, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+    }
+    class AppearanceSpan extends CharacterStyle {
+        int color;
+        public AppearanceSpan(int cntColor){
+            switch (cntColor){
+                case 0:
+                    color=Color.RED;
+                    break;
+                case 1:
+                    color=Color.BLUE;
+                    break;
+                default:
+                    color=Color.BLACK;
+
+            }
+        }
+
+        @Override
+        public void updateDrawState(TextPaint tp) {
+            tp.setColor(color);
+        }
     }
 
     @Override
@@ -80,36 +153,71 @@ public class PostDetailsActivity extends BaseActivity implements PostDetailsView
 
     @Override
     public void showPostDetails(Post post) {
+        post.setText(post.getText().replaceAll("<br>","<mybr>"));
         this.post=post;
-        shareLikeView.setiLikeable(post);
-        if(post.getRepost()==null) {
-            repost.setVisibility(GONE);
-            repost_photo.setImageDrawable(null);
-        }else {
-            repost.setVisibility(VISIBLE);
 
-            List<Photo> photos=post.getRepost().getPhoto();
-            if(photos!=null&&photos.size()>0&&photos.get(0).getUrlPreview()!=null)
-                repost_photo.post(() -> {
-                    float ratio = (float)photos.get(0).getSize().getWidth() / photos.get(0).getSize().getHeight();
-                    LinearLayout.LayoutParams params = ((LinearLayout.LayoutParams) repost_photo.getLayoutParams());
-                    params.height = (int) (repost_photo.getMeasuredWidth()/ratio);
-                    repost_photo.setLayoutParams(params);
-                    Picasso.with(PostDetailsActivity.this).load(photos.get(0).getUrl()).fit().centerCrop().into(repost_photo);
-                });
+        class FillContent{
+            public void fill(){
+                shareLikeView.setiLikeable(post);
+                repost();
+                texts();
+            }
 
-            repost_name.setText(post.getRepost().getUser_info()==null?"":post.getRepost().getUser_info().getFormattedName());
-            repost_text.setText(new StringBuilder()
-                    .append((post.getRepost().getShort_text()==null||post.getRepost().getShort_text().equals(""))?Html.fromHtml(String.valueOf(post.getRepost().getText())):post.getRepost().getShort_text())
-                    .toString()
-            );
+            private void texts() {
+                setTitle(post.getUser().getFormattedName());
+
+                title.setText(post.getUser().getFormattedName());
+                date.setText(DateTools.formatDottedDateWithTime(post.getDate()));
+
+                try {
+                    Spanned spannedText=Html.fromHtml(post.getText(),null,htmlTagHandler);
+                    Spannable reversedText = revertSpanned(spannedText);
+                    text.setText(reversedText);
+                }catch (Exception e){}
+
+            }
+            Spannable revertSpanned(Spanned stext) {
+                Object[] spans = stext.getSpans(0, stext.length(), Object.class);
+                Spannable ret = Spannable.Factory.getInstance().newSpannable(stext.toString());
+                if (spans != null && spans.length > 0) {
+                    for(int i = spans.length - 1; i >= 0; --i) {
+                        ret.setSpan(spans[i], stext.getSpanStart(spans[i]), stext.getSpanEnd(spans[i]), stext.getSpanFlags(spans[i]));
+                    }
+                }
+
+                return ret;
+            }
+
+            private void repost() {
+                if(post.getRepost()==null) {
+                    repost.setVisibility(GONE);
+                    repost_photo.setImageDrawable(null);
+                }else {
+                    repost.setVisibility(VISIBLE);
+
+                    List<Photo> photos=post.getRepost().getPhoto();
+                    if(photos!=null&&photos.size()>0&&photos.get(0).getUrlPreview()!=null)
+                        repost_photo.post(() -> {
+                            float ratio = (float)photos.get(0).getSize().getWidth() / photos.get(0).getSize().getHeight();
+                            LinearLayout.LayoutParams params = ((LinearLayout.LayoutParams) repost_photo.getLayoutParams());
+                            params.height = (int) (repost_photo.getMeasuredWidth()/ratio);
+                            repost_photo.setLayoutParams(params);
+                            Picasso.with(PostDetailsActivity.this).load(photos.get(0).getUrl()).fit().centerCrop().into(repost_photo);
+                        });
+
+                    repost_name.setText(post.getRepost().getUser_info()==null?"":post.getRepost().getUser_info().getFormattedName());
+                    repost_text.setText(new StringBuilder()
+                            .append((post.getRepost().getShort_text()==null||post.getRepost().getShort_text().equals(""))?Html.fromHtml(String.valueOf(post.getRepost().getText())):post.getRepost().getShort_text())
+                            .toString()
+                    );
+                }
+
+            }
         }
+        new FillContent().fill();
 
 
-        setTitle(post.getUser().getFormattedName());
-        title.setText(post.getUser().getFormattedName());
-        text.setText(post.getText() != null ? Html.fromHtml(post.getText()) : null);
-        date.setText(DateTools.formatDottedDateWithTime(post.getDate()));
+
     }
 
     @Override
--- app/src/main/java/com/brewmapp/presentation/view/impl/widget/PostView.java
+++ app/src/main/java/com/brewmapp/presentation/view/impl/widget/PostView.java
@@ -3,6 +3,7 @@ package com.brewmapp.presentation.view.impl.widget;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.text.Editable;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.AttributeSet;
@@ -21,6 +22,8 @@ import com.brewmapp.data.entity.Photo;
 import com.brewmapp.data.entity.Post;
 import com.brewmapp.execution.exchange.request.base.Keys;
 import com.squareup.picasso.Picasso;
+
+import org.xml.sax.XMLReader;
 
 import java.util.List;
 
@@ -48,8 +51,7 @@ public class PostView extends BaseLinearLayout implements InteractiveModelView<P
 
     private Listener listener;
     private Post model;
-    private int originalHeight=0;
-    //private FillContent fillContent = new FillContent();
+    private Html.TagHandler htmlTagHandler;
 
     public PostView(Context context) {
         super(context);
@@ -74,8 +76,15 @@ public class PostView extends BaseLinearLayout implements InteractiveModelView<P
         ButterKnife.bind(this);
         text.setMovementMethod(LinkMovementMethod.getInstance());
         container.setOnClickListener(v -> listener.onModelAction(Actions.ACTION_SELECT_POST, model));
-        text.setOnClickListener(v -> listener.onModelAction(Actions.ACTION_SELECT_POST, model));
+        //text.setOnClickListener(v -> listener.onModelAction(Actions.ACTION_SELECT_POST, model));
         repost.setOnClickListener(v -> listener.onModelAction(Actions.ACTION_SELECT_POST, model));
+        htmlTagHandler = new Html.TagHandler() {
+            @Override
+            public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+
+            }
+        };
+
     }
 
     @Override
@@ -86,10 +95,103 @@ public class PostView extends BaseLinearLayout implements InteractiveModelView<P
     @Override
     public void setModel(Post model) {
         this.model = model;
+
+        class FillContent {
+
+            String photoUrl=null;
+            int photoWidth=0;
+            int photoHeight=0;
+
+            private void image() {
+                post_photo.setImageBitmap(null);
+                try {photoUrl=model.getPhoto().get(0).getUrl();photoHeight=model.getPhoto().get(0).getSize().getHeight();photoWidth=model.getPhoto().get(0).getSize().getWidth();}catch (Exception e){photoUrl=null;}
+                if(photoUrl==null) {post_photo.setVisibility(GONE);return;}else {post_photo.setVisibility(VISIBLE);}
+                post_photo.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        post_photo.getViewTreeObserver().removeOnPreDrawListener(this);
+                        //resize
+                        float ratio=(float) post_photo.getWidth()/(float) photoWidth;
+                        LayoutParams p= (LayoutParams) post_photo.getLayoutParams();
+                        p.height=(int) (photoHeight*ratio);
+                        post_photo.setLayoutParams(p);
+                        //load
+                        post(() -> Picasso.with(getContext()).load(photoUrl).error(R.drawable.ic_default_resto).into(post_photo));
+
+                        return true;
+                    }
+                });
+            }
+
+            private void texts() {
+                try{text.setText(Html.fromHtml(model.getText(),null,htmlTagHandler));}catch (Exception e){}
+                date.setText(DateTools.formatDottedDateWithTime(model.getDate()));
+                try {author.setText(model.getRelated_model_data().getName());}catch (Exception e){}
+            }
+
+            private void avatar() {
+                String avatarLoad=null;
+                try {avatarLoad=model.getRelated_model_data().getGetThumb();}catch (Exception e){}
+                int avatarError;
+                switch (model.getRelated_model()){
+                    case Keys.CAP_RESTO:
+                        avatarError=R.drawable.ic_default_resto;
+                        break;
+                    default:
+                        avatarError=R.drawable.ic_default_resto;
+                }
+                if(avatarLoad==null)
+                    Picasso.with(getContext()).load(avatarError).fit().centerCrop().into(avatar);
+                else
+                    Picasso.with(getContext()).load(avatarLoad).fit().centerCrop().into(avatar);
+
+            }
+
+            private void repost() {
+                if(model.getRepost()==null) {
+                    repost.setVisibility(GONE);
+                }else {
+                    repost.setVisibility(VISIBLE);
+                    repost_photo.post(() -> {
+                        List<Photo> photos = model.getRepost().getPhoto();
+                        if (photos != null && photos.size() > 0 && photos.get(0).getUrlPreview() != null) {
+                            float ratio = (float) photos.get(0).getSize().getWidth() / photos.get(0).getSize().getHeight();
+                            LayoutParams params = ((LayoutParams) repost_photo.getLayoutParams());
+                            params.height = (int) (repost_photo.getMeasuredWidth() / ratio);
+                            repost_photo.setLayoutParams(params);
+                            repost_photo.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Picasso.with(getContext()).load(photos.get(0).getUrl()).fit().centerCrop().into(repost_photo);
+                                }
+                            });
+
+                        } else {
+                            repost_photo.setImageDrawable(null);
+                            LayoutParams params = ((LayoutParams) repost_photo.getLayoutParams());
+                            params.height = params.width = 0;
+                            repost_photo.setLayoutParams(params);
+                        }
+                    });
+                    repost_name.setText(model.getRepost().getUser_info()==null?"":model.getRepost().getUser_info().getFormattedName());
+                    repost_text.setText(new StringBuilder()
+                            .append((model.getRepost().getShort_text()==null||model.getRepost().getShort_text().equals(""))?Html.fromHtml(String.valueOf(model.getRepost().getText())):model.getRepost().getShort_text())
+                            .toString()
+                    );
+                }
+            }
+
+            public void fill() {
+                shareLikeView.setiLikeable(model);
+                repost();
+                avatar();
+                texts();
+                image();
+            }
+
+        }
+
         new FillContent().fill();
-
-
-
     }
 
     @Override
@@ -97,99 +199,5 @@ public class PostView extends BaseLinearLayout implements InteractiveModelView<P
         this.listener = listener;
     }
 
-    class FillContent {
-
-        String photoUrl=null;
-        int photoWidth=0;
-        int photoHeight=0;
-
-        private void setImage() {
-            post_photo.setImageBitmap(null);
-            try {photoUrl=model.getPhoto().get(0).getUrl();photoHeight=model.getPhoto().get(0).getSize().getHeight();photoWidth=model.getPhoto().get(0).getSize().getWidth();}catch (Exception e){photoUrl=null;}
-            if(photoUrl==null) {post_photo.setVisibility(GONE);return;}else {post_photo.setVisibility(VISIBLE);}
-            post_photo.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-                @Override
-                public boolean onPreDraw() {
-                    post_photo.getViewTreeObserver().removeOnPreDrawListener(this);
-                    //resize
-                    float ratio=(float) post_photo.getWidth()/(float) photoWidth;
-                    //post_photo.setMinimumHeight((int) (photoHeight*ratio));
-                    LayoutParams p= (LayoutParams) post_photo.getLayoutParams();
-                    p.height=(int) (photoHeight*ratio);
-                    post_photo.setLayoutParams(p);
-                    //load
-                    post_photo.postDelayed(() -> Picasso.with(getContext()).load(photoUrl).fit().centerCrop().into(post_photo),10);
-                    return true;
-                }
-            });
-        }
-
-        private void setTexts() {
-            text.setText(model.getText() != null ? Html.fromHtml(model.getText()) : null);
-            date.setText(DateTools.formatDottedDateWithTime(model.getDate()));
-            try {author.setText(model.getRelated_model_data().getName());}catch (Exception e){}
-        }
-
-        private void setAvatar() {
-            String avatarLoad=null;
-            try {avatarLoad=model.getRelated_model_data().getGetThumb();}catch (Exception e){}
-            int avatarError;
-            switch (model.getRelated_model()){
-                case Keys.CAP_RESTO:
-                    avatarError=R.drawable.ic_default_resto;
-                    break;
-                default:
-                    avatarError=R.drawable.ic_default_resto;
-            }
-            if(avatarLoad==null)
-                Picasso.with(getContext()).load(avatarError).fit().centerCrop().into(avatar);
-            else
-                Picasso.with(getContext()).load(avatarLoad).fit().centerCrop().into(avatar);
-
-        }
-
-        private void setRepost() {
-            if(model.getRepost()==null) {
-                repost.setVisibility(GONE);
-            }else {
-                repost.setVisibility(VISIBLE);
-                repost_photo.post(() -> {
-                    List<Photo> photos = model.getRepost().getPhoto();
-                    if (photos != null && photos.size() > 0 && photos.get(0).getUrlPreview() != null) {
-                        float ratio = (float) photos.get(0).getSize().getWidth() / photos.get(0).getSize().getHeight();
-                        LayoutParams params = ((LayoutParams) repost_photo.getLayoutParams());
-                        params.height = (int) (repost_photo.getMeasuredWidth() / ratio);
-                        repost_photo.setLayoutParams(params);
-                        repost_photo.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Picasso.with(getContext()).load(photos.get(0).getUrl()).fit().centerCrop().into(repost_photo);
-                            }
-                        });
-
-                    } else {
-                        repost_photo.setImageDrawable(null);
-                        LayoutParams params = ((LayoutParams) repost_photo.getLayoutParams());
-                        params.height = params.width = 0;
-                        repost_photo.setLayoutParams(params);
-                    }
-                });
-                repost_name.setText(model.getRepost().getUser_info()==null?"":model.getRepost().getUser_info().getFormattedName());
-                repost_text.setText(new StringBuilder()
-                        .append((model.getRepost().getShort_text()==null||model.getRepost().getShort_text().equals(""))?Html.fromHtml(String.valueOf(model.getRepost().getText())):model.getRepost().getShort_text())
-                        .toString()
-                );
-            }
-        }
-
-        public void fill() {
-            shareLikeView.setiLikeable(model);
-            setRepost();
-            setAvatar();
-            setTexts();
-            setImage();
-        }
-
-    }
 
 }

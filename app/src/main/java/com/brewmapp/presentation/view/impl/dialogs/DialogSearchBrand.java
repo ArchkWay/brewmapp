61e73957e934eed634dddb5a3f1bdc671de490d2
Kras <xpusher@yandex.ru>
2017-12-31 10:16:17 +0400

    dev

--- app/src/main/java/com/brewmapp/presentation/view/impl/dialogs/DialogSearchBrand.java
+++ app/src/main/java/com/brewmapp/presentation/view/impl/dialogs/DialogSearchBrand.java
@@ -2,6 +2,8 @@ package com.brewmapp.presentation.view.impl.dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,8 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.brewmapp.R;
 import com.brewmapp.app.environment.BeerMap;
@@ -23,6 +27,8 @@ import com.brewmapp.data.entity.wrapper.BeerBrandInfo;
 import com.brewmapp.data.pojo.FullSearchPackage;
 import com.brewmapp.execution.exchange.request.base.Keys;
 import com.brewmapp.execution.task.FullSearchTask;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.Iterator;
 import java.util.List;
@@ -40,12 +46,12 @@ import ru.frosteye.ovsa.stub.view.RefreshableSwipeRefreshLayout;
 public class DialogSearchBrand extends DialogFragment {
     private ListView lv;
     private SearchView sv;
-    private ArrayAdapter<String> adapter;
+    private CustomArrayAdapter adapter;
     private RefreshableSwipeRefreshLayout refreshableSwipeRefreshLayout;
     private FullSearchTask fullSearchTask;
     private FullSearchPackage fullSearchPackage;
     private OnSelectBrand onSelectBrand;
-    private String[] texts={};
+    private dataItem[] dataItems={};
     private List<IFlexible> iFlexiblesResult;
 
     public void show(FragmentManager fragmentManager,OnSelectBrand onSelectBrand){
@@ -93,10 +99,14 @@ public class DialogSearchBrand extends DialogFragment {
                         public void onNext(List<IFlexible> iFlexibles) {
                             super.onNext(iFlexibles);
                             iFlexiblesResult=iFlexibles;
-                            texts=new String[iFlexibles.size()];
-                            for (int i=0;i<iFlexibles.size();i++)
-                                texts[i]=((BeerBrandInfo)iFlexibles.get(i)).getModel().getName();
-                            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, texts);
+                            dataItems=new dataItem[iFlexibles.size()];
+                            for (int i=0;i<iFlexibles.size();i++) {
+                                dataItem dataItem=new dataItem();
+                                dataItem.text = ((BeerBrandInfo) iFlexibles.get(i)).getModel().getName();
+                                dataItem.image = ((BeerBrandInfo) iFlexibles.get(i)).getModel().getGetThumb();
+                                dataItems[i]=dataItem;
+                            }
+                            adapter = new CustomArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, dataItems);
                             lv.setAdapter(adapter);
                             refreshableSwipeRefreshLayout.setRefreshing(false);
                         }
@@ -122,14 +132,59 @@ public class DialogSearchBrand extends DialogFragment {
     }
 
     private void resetList() {
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, new String[0]);
+        adapter = new CustomArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, new dataItem[0]);
         lv.setAdapter(adapter);
         refreshableSwipeRefreshLayout.setRefreshing(false);
     }
 
-
     public interface OnSelectBrand{
         public void onSelect(BeerBrand beerBrand);
+    }
+    //***********************************************
+    class CustomArrayAdapter extends ArrayAdapter{
+        dataItem[] objects;
+
+        public CustomArrayAdapter(@NonNull Context context, int resource, @NonNull dataItem[] objects) {
+            super(context, resource, objects);
+            this.objects=objects;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            dataItem dataItem=dataItems[position];
+            if(convertView==null)
+                convertView= super.getView(position, convertView, parent);
+            ((TextView)convertView).setText(dataItem.text);
+            if(dataItem.image!=null) {
+                View finalConvertView = convertView;
+                Picasso.with(getActivity()).load(dataItem.image).into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        ImageView imageView=new ImageView(getActivity());
+                        imageView.setImageBitmap(bitmap);
+                        ((TextView) finalConvertView).setCompoundDrawablesWithIntrinsicBounds(null, null, null, imageView.getDrawable());
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });
+            }
+
+            return convertView;
+        }
+    }
+
+    class dataItem{
+        String text;
+        String image;
     }
 
 }

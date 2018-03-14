50e2676ce9e3c8feeed57e7c424e94d4a9f384f0
xpusher <xpusher@yandex.ru>
2018-03-14 15:49:04 +0400

    dev

---
+++ app/src/main/java/nl/psdcompany/duonavigationdrawer/views/CustomDuoDrawerLayout.java
@@ -0,0 +1,76 @@
+package nl.psdcompany.duonavigationdrawer.views;
+
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ScrollerCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by xpusher on 3/14/2018.
+ */
+
+public class CustomDuoDrawerLayout extends DuoDrawerLayout {
+    public CustomDuoDrawerLayout(Context context) {
+        super(context);
+    }
+
+    public CustomDuoDrawerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomDuoDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+    @Override
+    public void openDrawer() {
+        super.openDrawer();
+
+        try {
+            Field f_mViewDragHelper=getClass().getSuperclass().getDeclaredField("mViewDragHelper");
+            f_mViewDragHelper.setAccessible(true);
+            Object o_mViewDragHelper=f_mViewDragHelper.get(this);
+
+            Field f_mScroller=o_mViewDragHelper.getClass().getDeclaredField("mScroller");
+            f_mScroller.setAccessible(true);
+            ScrollerCompat mScroller= (ScrollerCompat) f_mScroller.get(o_mViewDragHelper);
+
+            mScroller.abortAnimation();
+            ViewCompat.postInvalidateOnAnimation(CustomDuoDrawerLayout.this);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void closeDrawer() {
+        super.closeDrawer();
+        try {
+            Field f_mViewDragHelper=getClass().getSuperclass().getDeclaredField("mViewDragHelper");
+            f_mViewDragHelper.setAccessible(true);
+            Object o_mViewDragHelper=f_mViewDragHelper.get(this);
+
+            Field f_mScroller=o_mViewDragHelper.getClass().getDeclaredField("mScroller");
+            f_mScroller.setAccessible(true);
+            ScrollerCompat mScroller= (ScrollerCompat) f_mScroller.get(o_mViewDragHelper);
+
+            mScroller.abortAnimation();
+            ViewCompat.postInvalidateOnAnimation(CustomDuoDrawerLayout.this);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
